68cd48e6529120b1b5254a798b87668f485ee9bd
manabshy <manabshy@gmail.com>
2019-04-12 16:10:02 +0100

    update module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { FilmsModule } from './films/films.module';
 import { GhibliFilmsModule } from './ghibli-films/ghibli-films.module';
 
 @Module({
-  imports: [FilmsModule, GhibliFilmsModule],
+  imports: [GhibliFilmsModule],
   controllers: [AppController],
   providers: [AppService],
 })
