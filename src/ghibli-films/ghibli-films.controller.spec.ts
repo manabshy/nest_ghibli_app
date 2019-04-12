849895c7eda8f15062cc21e35a537261a8dcab20
manabshy <manabshy@gmail.com>
2019-04-12 16:25:23 +0100

    Add testing|Fix tests

--- src/ghibli-films/ghibli-films.controller.spec.ts
+++ src/ghibli-films/ghibli-films.controller.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GhibliFilmsController } from './ghibli-films.controller';
+import { GhibliFilmsService } from './ghibli-films.service';
 
 describe('GhibliFilms Controller', () => {
   let controller: GhibliFilmsController;
+  let ghibliFilmsService: GhibliFilmsService;
 
   beforeEach(async () => {
+    ghibliFilmsService = new GhibliFilmsService();
+    controller = new GhibliFilmsController(ghibliFilmsService);
     const module: TestingModule = await Test.createTestingModule({
       controllers: [GhibliFilmsController],
+      providers: [GhibliFilmsService],
     }).compile();
 
     controller = module.get<GhibliFilmsController>(GhibliFilmsController);
@@ -15,4 +20,5 @@ describe('GhibliFilms Controller', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
 });
